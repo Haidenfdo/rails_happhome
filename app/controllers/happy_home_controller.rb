d5cc5e9c19b4bda8b50bd8cddb60c4661fd04428
Haiden Arulappan <you@example.com>
2022-07-11 15:30:53 +0530

    add

--- app/controllers/happy_home_controller.rb
+++ app/controllers/happy_home_controller.rb
@@ -1,4 +1,14 @@
 class HappyHomeController < ApplicationController
+
   def index
+
+  end
+
+  def sign_up
+     
+  end
+
+  def login
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,17 @@ Rails.application.routes.draw do
 
   root 'happy_home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  match'happy_home/index',:to=>'happy_home#index',:via=>:get
+
+  match'happy_home/sign_up',:to=>'happy_home#sign_up',:via=>:get
+
+  match'happy_home/login',:to=>'happy_home#login',:via=>:get
+
+  match'/signup',:to=>'happy_home#sign_up',:via=>:get
+
+  match'/login',:to=>'happy_home#login',:via=>:get
+
+  match'/index',:to=>'happy_home#index',:via=>:get
+
+  
 end
